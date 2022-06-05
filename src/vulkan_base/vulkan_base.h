3f2723e4ff9f7e5a90bc5240dee3c272544394bc
Dominik Szczepek <dszczepek@outlook.de>
2022-06-05 15:02:31 +0200

    Show the application layers
    To check which layers are present in the current context.

--- src/vulkan_base/vulkan_base.h
+++ src/vulkan_base/vulkan_base.h
@@ -1,5 +1,16 @@
 
 #include <vulkan/vulkan.h>
+#include <cassert>
+
+#define ASSERT_VULKAN(val) if(val != VK_SUCCESS){ assert(false); }
+#ifndef VK
+#define VK(f) (f)
+#endif // !VK
+#ifndef VKA
+#define VKA(f) ASSERT_VULKAN(VK(f))
+#endif // !VKA
+
+#define ARRAY_COUNT(array) (sizeof(array) / sizeof((array)[0]))
 
 struct VulkanContext
 {
--- src/vulkan_base/vulkan_device.cpp
+++ src/vulkan_base/vulkan_device.cpp
@@ -1,8 +1,23 @@
 #include "vulkan_base.h"
 
 #include <SDL.h>
+#include <iostream>
 
 bool initVulkanInstance(VulkanContext* context) {
+	uint32_t layerPropertyCount = 0;
+	VKA(vkEnumerateInstanceLayerProperties(&layerPropertyCount, 0));
+	VkLayerProperties* layerProperties = new VkLayerProperties[layerPropertyCount];
+	VKA(vkEnumerateInstanceLayerProperties(&layerPropertyCount, layerProperties));
+	for (uint32_t i = 0; i < layerPropertyCount; ++i)
+	{
+		std::cout << layerProperties[i].layerName << std::endl;
+		std::cout << layerProperties[i].description << std::endl;
+	}
+
+	const char* enabledLayers[] = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+
 	VkApplicationInfo applicationInfo = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
 	applicationInfo.pApplicationName = "Vulkan-Testing-Grounds";
 	applicationInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
@@ -12,8 +27,10 @@ bool initVulkanInstance(VulkanContext* context) {
 
 	VkInstanceCreateInfo createInfo = { VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
 	createInfo.pApplicationInfo = &applicationInfo;
+	createInfo.enabledLayerCount = ARRAY_COUNT(enabledLayers);
+	createInfo.ppEnabledLayerNames = enabledLayers;
 	
-	if (vkCreateInstance(&createInfo, 0, &context->instance) != VK_SUCCESS)
+	if (VK(vkCreateInstance(&createInfo, 0, &context->instance)) != VK_SUCCESS)
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", "Vulkan was not created!", NULL);
 		return false;
