d7de502905211c94283943daeafe6a11ec22bad6
Dominik Szczepek <dszczepek@outlook.de>
2022-06-09 19:37:28 +0200

    Set up render pass
    For rendering into the window.

--- src/vulkan_base/vulkan_swapchain.cpp
+++ src/vulkan_base/vulkan_swapchain.cpp
@@ -50,11 +50,27 @@ VulkanSwapchain createSwapchain(VulkanContext* context, VkSurfaceKHR surface, Vk
 	createInfo.presentMode = VK_PRESENT_MODE_FIFO_KHR;
 	VKA(vkCreateSwapchainKHR(context->device, &createInfo, 0, &result.swapchain));
 
+	result.format = format;
+	result.width = surfaceCapabilities.currentExtent.width;
+	result.height = surfaceCapabilities.currentExtent.height;
+
 	uint32_t numImages;
 	VKA(vkGetSwapchainImagesKHR(context->device, result.swapchain, &numImages, 0));
 
 	result.images.resize(numImages);
 	VKA(vkGetSwapchainImagesKHR(context->device, result.swapchain, &numImages, result.images.data()));
+
+	result.imageViews.resize(numImages);
+	for (uint32_t i = 0; i < numImages; ++i)
+	{
+		VkImageViewCreateInfo createInfo = { VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO };
+		createInfo.image = result.images[i];
+		createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		createInfo.format = format;
+		createInfo.components = {};
+		createInfo.subresourceRange = { VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 };
+		VKA(vkCreateImageView(context->device, &createInfo, 0, &result.imageViews[i]));
+	}
 
 	delete[] availableFormats;
 
@@ -62,5 +78,9 @@ VulkanSwapchain createSwapchain(VulkanContext* context, VkSurfaceKHR surface, Vk
 }
 
 void destroySwapchain(VulkanContext* context, VulkanSwapchain* swapchain) {
+	for (uint32_t i = 0; i < swapchain->imageViews.size(); ++i)
+	{
+		VK(vkDestroyImageView(context->device, swapchain->imageViews[i], 0));
+	}
 	VK(vkDestroySwapchainKHR(context->device, swapchain->swapchain, 0));
 }
