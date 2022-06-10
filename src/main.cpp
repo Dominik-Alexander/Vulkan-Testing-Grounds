7cfc577931712040a0e9047b4250090ac6a5b76e
Dominik Szczepek <dszczepek@outlook.de>
2022-06-10 17:21:56 +0200

    Refactor main
    Split it up in initialization, run and shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,13 @@
 
 #include "vulkan_base/vulkan_base.h"
 
-int main() {
+VulkanContext* context;
+VkSurfaceKHR surface;
+VkRenderPass renderPass;
+VulkanSwapchain swapchain;
+std::vector<VkFramebuffer> framebuffers;
 
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
-	{
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", SDL_GetError(), NULL);
-
-		return 1;
-	}
-
-	SDL_Window* window = SDL_CreateWindow("Hello, Vulkan!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_VULKAN);
-
+void initApplication(SDL_Window* window) {
     uint32_t instanceExtensionCount = 0;
     SDL_Vulkan_GetInstanceExtensions(window, &instanceExtensionCount, 0);
     const char** enabledInstanceExtensions = new const char* [instanceExtensionCount];
@@ -22,13 +18,11 @@ int main() {
 
     const char* enabledDeviceExtensions[]{ VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 
-    VulkanContext* context = initVulkan(instanceExtensionCount, enabledInstanceExtensions, ARRAY_COUNT(enabledDeviceExtensions), enabledDeviceExtensions);
-    VkSurfaceKHR surface;
+    context = initVulkan(instanceExtensionCount, enabledInstanceExtensions, ARRAY_COUNT(enabledDeviceExtensions), enabledDeviceExtensions);
     SDL_Vulkan_CreateSurface(window, context->instance, &surface);
-    VulkanSwapchain swapchain = createSwapchain(context, surface, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
+    swapchain = createSwapchain(context, surface, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
 
-    VkRenderPass renderPass = createRenderPass(context, swapchain.format);
-    std::vector<VkFramebuffer> framebuffers;
+    renderPass = createRenderPass(context, swapchain.format);
     framebuffers.resize(swapchain.images.size());
     for (uint32_t i = 0; i < swapchain.images.size(); ++i)
     {
@@ -41,11 +35,42 @@ int main() {
         createInfo.layers = 1;
         VKA(vkCreateFramebuffer(context->device, &createInfo, 0, &framebuffers[i]));
     }
+}
+
+void renderApplication() {
+
+}
+
+void shutdownApplication() {
+    VKA(vkDeviceWaitIdle(context->device));
+    for (uint32_t i = 0; i < framebuffers.size(); ++i)
+    {
+        VK(vkDestroyFramebuffer(context->device, framebuffers[i], 0));
+    }
+    framebuffers.clear();
+    destroyRenderpass(context, renderPass);
+    destroySwapchain(context, &swapchain);
+    VK(vkDestroySurfaceKHR(context->instance, surface, 0));
+    exitVulkan(context);
+}
+
+int main() {
+
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", SDL_GetError(), NULL);
+
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Hello, Vulkan!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_VULKAN);
 
 	if (!window)
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", SDL_GetError(), NULL);
 	}
+
+    initApplication(window);
 
     SDL_Event event;
 
@@ -70,21 +95,13 @@ int main() {
             }
             default:
             {
+                renderApplication();
                 break;
             }
         }
     }
 
-    VKA(vkDeviceWaitIdle(context->device));
-    for (uint32_t i = 0; i < framebuffers.size(); ++i)
-    {
-        VK(vkDestroyFramebuffer(context->device, framebuffers[i], 0));
-    }
-    framebuffers.clear();
-    destroyRenderpass(context, renderPass);
-    destroySwapchain(context, &swapchain);
-    VK(vkDestroySurfaceKHR(context->instance, surface, 0));
-    exitVulkan(context);
+    shutdownApplication();
 
 	SDL_DestroyWindow(window);
 	SDL_Quit();
