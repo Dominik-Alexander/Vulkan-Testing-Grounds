91ae7a9350c4ba3a554204b091172f686825dad3
Dominik Szczepek <dszczepek@outlook.de>
2022-06-11 20:56:20 +0200

    Create the pipeline and create the triangle
    The first triangle is created with the help of a vertex and a fragment shader.

--- shaders/triangle_frag.glsl
+++ shaders/triangle_frag.glsl
@@ -1,7 +1,9 @@
 #version 450 core
 
+layout(location = 0) in vec3 vertex_color;
+
 layout(location = 0) out vec4 color_out;
 
 void main() {
-	color_out = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	color_out = vec4(vertex_color, 1.0);
 }
